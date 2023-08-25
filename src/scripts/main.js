bbaf68e5615496e7f8ef9f4dd4e604a5270518db
Paulo Andre <andreairesw@gmail.com>
2023-08-25 14:45:28 -0300

    constrindo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("ola mundo");
+// JavaScript para tornar o botão de confirmação responsivo
+const submitButton = document.querySelector('button[type="submit"]');
+const form = document.querySelector('form');
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    alert('Obrigado por confirmar sua presença!');
+    form.reset();
+});
